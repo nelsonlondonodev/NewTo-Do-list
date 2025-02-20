56dfa0fd4de19b773e542aa337a73e8261c49c11
Nelson Londoño <nelsonlondonodev@gmail.com>
2025-02-20 21:28:10 +0100

    Create class for li and print value in screen

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const dataInput = document.querySelector("#inputNewTask");
-const getValue = dataInput.value.trim();
-
-console.log(getValue);
+dataInput.addEventListener("input", () => {
+  console.log(dataInput.value);
+});
 
 function buttonNetask() {
   const ulList = document.getElementById("ul-list");
   const newList = document.createElement("li");
+  newList.classList.add("shadow", "p-3", "mt-1", "mb-3", "bg-body", "rounded");
   ulList.appendChild(newList);
 
-  console.log("Conecto con la función de crear li...");
+  newList.innerText = dataInput.value;
+
+  dataInput.value = "";
 }
