084f870f2aa6f6d0181b8c7ce5cfa93c004e3503
Nelson Londoño <nelsonlondonodev@gmail.com>
2025-02-20 21:48:19 +0100

    Create a if and else

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 const dataInput = document.querySelector("#inputNewTask");
 dataInput.addEventListener("input", () => {
-  console.log(dataInput.value);
+  return dataInput.value;
+  // console.log(dataInput.value);
 });
 
 function buttonNetask() {
-  const ulList = document.getElementById("ul-list");
-  const newList = document.createElement("li");
-  newList.classList.add("shadow", "p-3", "mt-1", "mb-3", "bg-body", "rounded");
-  ulList.appendChild(newList);
+  let click = true;
 
-  newList.innerText = dataInput.value;
+  if (click) {
+    const ulList = document.getElementById("ul-list");
+    const newList = document.createElement("li");
+    newList.classList.add(
+      "shadow",
+      "p-3",
+      "mt-1",
+      "mb-3",
+      "bg-body",
+      "rounded"
+    );
 
-  dataInput.value = "";
+    ulList.appendChild(newList);
+    newList.innerText = dataInput.value;
+    dataInput.value = "";
+  } else {
+    const divH3 = document.querySelector(".divh3");
+    divH3.classList.add("invisible");
+  }
 }
