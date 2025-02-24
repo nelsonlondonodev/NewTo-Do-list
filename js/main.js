5e16ae3c013f7500decba1ccdaeeea10908ebc3f
Nelson Londoño <nelsonlondonodev@gmail.com>
2025-02-24 06:58:42 +0100

    Put elements in new "li"

--- js/main.js
+++ js/main.js
@@ -12,37 +12,6 @@ function buttonNewtask(_id) {
   if (click) {
     const ulList = document.getElementById("ul-list");
     const newList = document.createElement("li");
-
-    const onclickLi = "clickLi" + Math.random().toString(36).substring(2, 9);
-
-    newList.id = "li-id-" + Math.random().toString(36).substring(2, 9);
-
-    function elementsLi() {
-      //== Image ok
-      const toCreateImgOk = document.createElement("img");
-      toCreateImgOk.src = "/images/icons8-de-acuerdo-94.png";
-      toCreateImgOk.width = 50;
-      toCreateImgOk.height = 50;
-      toCreateImgOk.alt = "Image ok";
-      toCreateImgOk.style.zIndex = 10;
-      ulList.appendChild(toCreateImgOk);
-
-      console.log("Conecto con imagen 1...");
-
-      //== Image delet
-      const toCreateImgDelet = document.createElement("img");
-      toCreateImgDelet.src = "/images/icons8-basura-94.png";
-      toCreateImgDelet.width = 50;
-      toCreateImgDelet.height = 50;
-      toCreateImgDelet.alt = "Image ok";
-      toCreateImgDelet.style.zIndex = 10;
-      ulList.appendChild(toCreateImgDelet);
-
-      console.log("Conecto con imagen 2...");
-
-      return;
-    }
-
     newList.classList.add(
       "shadow",
       "text-center",
@@ -53,9 +22,40 @@ function buttonNewtask(_id) {
       "rounded"
     );
 
+    const onclickLi = "clickLi" + Math.random().toString(36).substring(2, 9);
+
+    newList.id = "li-id-" + Math.random().toString(36).substring(2, 9);
+
+    //== Image ok
+    const toCreateImgOk = document.createElement("img");
+    toCreateImgOk.src = "/images/icons8-de-acuerdo-94.png";
+    toCreateImgOk.width = 50;
+    toCreateImgOk.height = 50;
+    toCreateImgOk.alt = "Image ok";
+    toCreateImgOk.style.zIndex = 10;
+    ulList.appendChild(toCreateImgOk);
+
+    console.log("Conecto con imagen 1...");
+
+    //== Image delet
+    const toCreateImgDelet = document.createElement("img");
+    toCreateImgDelet.src = "/images/icons8-basura-94.png";
+    toCreateImgDelet.width = 50;
+    toCreateImgDelet.height = 50;
+    toCreateImgDelet.alt = "Image ok";
+    toCreateImgDelet.style.zIndex = 10;
+    ulList.appendChild(toCreateImgDelet);
+
+    console.log("Conecto con imagen 2...");
+
+    newList.appendChild(toCreateImgOk);
+    newList.appendChild(dataInput);
+    newList.appendChild(toCreateImgDelet);
+
     ulList.appendChild(newList);
-    newList.innerText = dataInput.value;
-    dataInput.value = "";
+    // newList.innerText = dataInput.value;
+    // dataInput.value = "";
+    document.querySelector("#inputNewTask").value = "";
 
     const divH3 = document.querySelector(".divh3");
     divH3.classList.replace("visible", "invisible");
@@ -65,7 +65,6 @@ function buttonNewtask(_id) {
       let click = true;
 
       if (click) {
-        elementsLi();
         console.log("Entro el click");
         return;
       }
